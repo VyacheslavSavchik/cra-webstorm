4b536671cf8c87d0114467a802b4159267b5b9bb
VyacheslavSavchik <rothmans.830@gmail.com>
2022-10-09 23:53:53 +0300

    Commit - 2

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <p>Lorem ipsum dolor.</p>
+        <p>Lorem ipsum dolor.11</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
